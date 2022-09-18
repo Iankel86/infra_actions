274afc56aada2b0d78ad6e3a41e66453efe63c06
Ян Шадрин <Yankelll86@mail.ru>
2022-09-18 23:21:33 +0300

    дополнил main.yml-исправляю ощибки-1

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -6,6 +6,6 @@ app_name = 'infra_app'
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='/'),
     path('second/', views.second_page, name='second_page'),
 ]
